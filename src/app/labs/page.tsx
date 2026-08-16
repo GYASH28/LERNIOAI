01df4b82587c79252ea05c5fbc913abc50b407bc
GYASH28 <yash.k.ganesh@gmail.com>
2026-08-16 13:43:50 +0530

    chore: retire diploma labs route

--- src/app/labs/page.tsx
+++ src/app/labs/page.tsx
@@ -1,26 +1,5 @@
 import { redirect } from 'next/navigation'
-import { getCurrentUser } from '@/lib/auth'
-import { LabsView } from '@/components/views/labs'
-import { TopBar } from '@/components/layout/top-bar'
-import { Footer } from '@/components/layout/footer'
 
-export const dynamic = 'force-dynamic'
-
-export default async function LabsPage() {
-  const user = await getCurrentUser()
-  if (!user) redirect('/sign-in?callbackUrl=/labs')
-
-  return (
-    <div className="min-h-screen flex flex-col bg-background text-foreground">
-      <TopBar />
-      <main className="flex-1 min-w-0 flex flex-col">
-        <div className="flex-1">
-          <div className="mx-auto max-w-5xl px-4 py-6 sm:px-6 lg:px-8">
-            <LabsView />
-          </div>
-        </div>
-        <Footer />
-      </main>
-    </div>
-  )
+export default function LabsPage() {
+  redirect('/learn')
 }
